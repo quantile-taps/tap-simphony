e04d5c4bfab73c58660e2d083d760af1cf388ea2
Paulo Rijnberg <paulo.rijnberg@quantile.nl>
2025-03-12 08:31:50 +0100

    Remove other params

--- tap_simphony/streams.py
+++ tap_simphony/streams.py
@@ -195,7 +195,5 @@ class GuestChecks(SimphonyStream):
             "locRef": self.config["location_reference"],
 
             # TODO: add state logic to loop over the dates. By e.g. doing a sliding window.
-            "busDt": "2025-03-05",
-            # "opnBusDt": "2025-03-05",
-            # "clsdBusDt": "2025-03-06",
+            "busDt": "2025-03-01",
         }
