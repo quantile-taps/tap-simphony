f5c6861c847574e96f96377944c6231898c951ca
Paulo Rijnberg <paulo.rijnberg@quantile.nl>
2025-03-12 08:06:43 +0100

    Filtered streams

--- tap_simphony/tap.py
+++ tap_simphony/tap.py
@@ -49,10 +49,10 @@ class TapSimphony(Tap):
         return [
             streams.MenuItemPrices(self),
             streams.MenuItemDimensions(self),
-            streams.TaxDimensions(self),
-            streams.RevenueCenterDimensions(self),
-            streams.WorkstationsDimensions(self),
-            streams.GuestChecks(self),
+            # streams.TaxDimensions(self),
+            # streams.RevenueCenterDimensions(self),
+            # streams.WorkstationsDimensions(self),
+            # streams.GuestChecks(self),
         ]
 
 
